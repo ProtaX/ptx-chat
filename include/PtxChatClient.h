05a63ecfbbab0dc86d7460676683383ec99b9eff
Arseniy Karpenko <aka@zelax.ru>
2020-09-14 00:08:23 +0300

    client interface update

--- include/PtxChatClient.h
+++ include/PtxChatClient.h
@@ -9,6 +9,8 @@
 #include <memory>
 #include <deque>
 #include <utility>
+#include <fstream>
+#include <mutex>
 
 #include "Message.h"
 #include "Threads.h"
@@ -17,6 +19,7 @@ namespace ptxchat {
 
 constexpr uint32_t DEFAULT_SERVER_IP = 2130706433;
 constexpr uint16_t DEFAULT_SERVER_PORT = 1488;
+constexpr uint64_t MSG_THREAD_SLEEP = 100;
 
 class PtxChatClient {
  public:
@@ -26,13 +29,15 @@ class PtxChatClient {
 
   void LogIn(const std::string& nickname);
   void LogOut();
-  void SendPrivateMsg(const std::string& to, const std::string& text);
-  void SendPublicMsg(const std::string& text);
+  void SendMsgTo(const std::string& to, const std::string& text);
+  void SendMsg(const std::string& text);
 
  private:
   uint32_t server_ip_;     /**< Chat server ip (default=127.0.0.1) */
   uint16_t server_port_;   /**< Chat server port (default=1488) */
   std::string nick_;       /**< Nickname on server if logged in */
+  std::ofstream chat_log_;
+  std::mutex chat_log_mtx_;
 
   int socket_;
   struct sockaddr_in serv_addr_;
@@ -42,9 +47,12 @@ class PtxChatClient {
   std::deque<std::unique_ptr<struct ChatMsg>> msg_in_;
   std::deque<std::unique_ptr<struct ChatMsg>> msg_out_;
 
-  void SendServerMsg(std::unique_ptr<struct ChatMsg>&& msg);
+  void SendMsg(std::unique_ptr<struct ChatMsg>&& msg);
+
   void ReceiveMessages();
   void SendMessages();
+  void InitLog();
+  void Log(const std::string& text);
 };
 
 }  // namespace ptxchat
